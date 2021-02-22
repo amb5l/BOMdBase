738584c0d9cdfb7925a169f70f20923adc13b11a
Adam Barnes <ambarnes@gmail.com>
2021-02-22 21:09:39 +0000

    Handle untagged commits

--- BOMdBase/apps/main/views.py
+++ BOMdBase/apps/main/views.py
@@ -6,10 +6,12 @@ from .shared import navbar1
 
 def main(request):
     repo = git.Repo()
-    tag = str(repo.tags[-1])
     sha = repo.head.commit.hexsha
     short_sha = repo.git.rev_parse(sha, short=6)
-    notes = 'Version: ' + tag + '('+ short_sha + ')'
+    if repo.tags:
+        notes = 'Version: ' + str(repo.tags[-1]) + ' (Build '+ short_sha + ')'
+    else:
+        notes = 'Build: ' + short_sha
     context = { \
         'navbar1': navbar1,
         'navbar2': [
