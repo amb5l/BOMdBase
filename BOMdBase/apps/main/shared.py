0ed93ad036a3ce431ad28389c1b990a602ad0dc0
Adam Barnes <ambarnes@gmail.com>
2022-09-15 14:18:27 +0100

    Better default sorting of Supplier Parts

--- BOMdBase/apps/main/shared.py
+++ BOMdBase/apps/main/shared.py
@@ -3,7 +3,7 @@ navbar1 = [
     ('BOMs', '/boms/?sort=name'),
     ('Logical Parts', '/logical_parts/?sort=lpcat&sort=lpn'),
     ('Manufacturer Parts', '/manufacturer_parts/?sort=mpcat&sort=mporg&sort=mpn'),
-    ('Supplier Parts', '/supplier_parts/?sort=spcat&sort=mporg&sort=mpn&sort=sporg'),
+    ('Supplier Parts', '/supplier_parts/?sort=sporg&sort=spn'),
     ('Organizations', '/organizations/?sort=name')
 ]
 
